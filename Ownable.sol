afe613bbf1068faed40985beb02f16c212d72e98
chuhe <3568001975@qq.com>
2024-01-12 15:11:43 +0800

    update

--- Ownable.sol
+++ Ownable.sol
@@ -5,11 +5,24 @@ interface IBank {
     function withdraw() external ;
 }
 
+
 contract Ownable {
-    function withdraw(address bank) public  {
+    address owner;
+     constructor(address _owner) {
+        owner = _owner;
+    }
+    modifier ownerCheck(){
+        require(owner == msg.sender, "must be owner");
+        _;
+    }
+    function withdraw(address bank) public  ownerCheck{
         IBank(bank).withdraw();
-        // address payable ownable = payable (msg.sender);
-        // ownable.transfer(msg.value);
+        // 把钱转给用户
+        address payable sender = payable (msg.sender);
+        (bool success,) = sender.call{value:address(this).balance}("");
+        if(!success){
+            revert();
+        }
     }
 
     // 只要收钱就一定要有receive函数
